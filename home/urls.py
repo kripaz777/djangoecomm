edb478773ba3affee149a3cdf88cef33695fe40b
Aayush <aceraayush@gmail.com>
2022-11-22 11:00:49 +0545

    category

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,10 +4,11 @@ from django.views.generic import View
 # Create your views here.
 class BaseView(View):
     views = {}
+    views['categories'] = Category.objects.all()
 
 class HomeView(BaseView):
     def get(self,request):
-        self.views['categories'] = Category.objects.all()
+        self.views
         self.views['sliders'] = Slider.objects.all()
         self.views['ads'] = Ad.objects.all()
         self.views['brands'] = Brand.objects.all()
@@ -15,3 +16,10 @@ class HomeView(BaseView):
         self.views['hots'] = Product.objects.filter(labels='hot')
         self.views['sales'] = Product.objects.filter(labels='sale')
         return render(request,'index.html',self.views)
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        self.views
+        ids = Category.objects.get(slug = slug).id
+        self.views['catproducts'] = Product.objects.filter(category_id = ids)
+        return render(request, 'category.html', self.views)
