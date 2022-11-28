69652cea8c05bda0f76b3b1549e35b7e87b4a4ca
Aayush <aceraayush@gmail.com>
2022-11-28 07:52:31 +0545

    signup

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product_details/<slug>', ProductDetailView.as_view(), name='product_details'),
     path('product_review', product_review, name='product_review'),
     path('search_product', SearchProduct.as_view(), name='search_product'),
+    path('signup', signup, name='signup'),
 ]
